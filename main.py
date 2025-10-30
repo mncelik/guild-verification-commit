22333b65d3637d8d0ec3d8bfbdf564bd65e1a741
mncelik <38618868+mncelik@users.noreply.github.com>
2025-10-30 21:57:29 +0300

    Patch API endpoint error handling

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-10-30T18:56:54.003Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2025-10-30T18:57:28.801Z
+Commit: Patch API endpoint error handling
